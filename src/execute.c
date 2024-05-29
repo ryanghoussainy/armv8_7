45373643e86eedc4f7e62d14d008938878b1e2f2
ab4623 <ab4623@ic.ac.uk>
2024-05-29 16:21:38 +0100

    fixed execute_instruction (op0 bits were being read in the wrong order)

--- src/execute.c
+++ src/execute.c
@@ -2,18 +2,19 @@
 #include "dp-imm.h"
 #include "transfer.h"
 #include "branch.h"
+#include "masks.h"
 
 #define OP0_BITS 4
 
 int execute_instruction(struct CPU* cpu, uint32_t instruction)
 {
-    uint64_t op0 = (make_mask(25, 28) & instruction) >> 25;
+    uint64_t op0 = (build_mask(25, 28) & instruction) >> 25;
     uint64_t op0_bits[OP0_BITS];
 
     for (int bit = OP0_BITS - 1; bit >= 0; bit--) {
-        uint64_t mask = mask_mask(bit, bit);
+        uint64_t mask = build_mask(bit, bit);
         uint64_t bit_value = (op0 & mask) >> bit;
-        op0_bits[bit] = bit_value;
+        op0_bits[OP0_BITS - bit - 1] = bit_value;
     }
 
     if (op0_bits[0] == 1 && op0_bits[1] == 0 && op0_bits[2] == 0) {
