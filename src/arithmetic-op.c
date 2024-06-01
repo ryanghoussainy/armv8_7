ec1174cd12d55bb29add497f75484c737a4d10f0
Ryan Ghoussainy <rg1523@oak36.doc.ic.ac.uk>
2024-06-02 00:14:31 +0100

    Changed INT32_MAX to UINT32_MAX in arithmetic-op

--- src/arithmetic-op.c
+++ src/arithmetic-op.c
@@ -14,12 +14,8 @@ uint64_t arithmetic_operation(
             return Rn + op2;
         case 1: // Add, set flags
             result = Rn + op2;
-
-            if (sf == 0) {
-                result = result & INT32_MAX;
-            }
             
-            msb_result = sf ? result >> 63 : result >> 31;
+            msb_result = sf ? result >> 63 : (result & UINT32_MAX) >> 31;
             msb_Rn = sf ? Rn >> 63 : Rn >> 31;
             msb_op2 = sf ? op2 >> 63 : op2 >> 31;
 
@@ -28,21 +24,17 @@ uint64_t arithmetic_operation(
             // Z flag
             set_flag(cpu, Z, result == 0);
             // C flag
-            if (sf) {
-                set_flag(cpu, C, result < Rn || result < op2);
-            } else {
-                set_flag(cpu, C, result > INT32_MAX);
-            }
+            set_flag(cpu, C, sf ? result < Rn || result < op2 : result > UINT32_MAX);
             // V flag
             set_flag(cpu, V, msb_Rn == msb_op2 && msb_Rn != msb_result);
 
-            return result;
+            return sf ? result : result & UINT32_MAX;
         case 2: // Subtract
             return Rn - op2;
         case 3: // Subtract, set flags
             result = Rn - op2;
 
-            msb_result = sf ? result >> 63 : result >> 31;
+            msb_result = sf ? result >> 63 : (result & UINT32_MAX) >> 31;
             msb_Rn = sf ? Rn >> 63 : Rn >> 31;
             msb_op2 = sf ? op2 >> 63 : op2 >> 31;
 
