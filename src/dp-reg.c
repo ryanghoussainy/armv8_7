0a5663e279495fab3a79a7de01ef52cbdafc3dba
= <=>
2024-06-01 19:14:36 +0100

    Changed miss classification of an if

--- src/dp-reg.c
+++ src/dp-reg.c
@@ -52,7 +52,7 @@ int dp_reg_instruction(struct CPU* cpu, uint32_t instr) {
     struct DPRegComponents components = get_dp_reg_components(instr);
 
     if (components.M == 0) {
-        if (components.opr >= 8 && components.opr % 2) {
+        if (components.opr >= 8 && (components.opr % 2 == 0)) {
             return reg_arithmetic(cpu, &components);
         } else if (components.opr < 8) {
 
