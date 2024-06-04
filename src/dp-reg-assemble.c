b14a46e748f7d65eb7359b0f84397a0cd12b9f67
Ryan Ghoussainy <rg1523@oak25.doc.ic.ac.uk>
2024-06-04 22:29:20 +0100

    Changed functions to only pass pointer to struct

--- src/dp-reg-assemble.c
+++ src/dp-reg-assemble.c
@@ -1,10 +1,10 @@
 #include "dp-reg-assemble.h"
 
 
-static uint32_t build_dp_reg_arithmetic(Instruction instr) {
+static uint32_t build_dp_reg_arithmetic(Instruction* instr) {
     // opc - bits 29-30
     uint32_t opc;
-    char* op = instr.operation;
+    char* op = instr->operation;
     if (strcmp(op, "add") == 0) {
         opc = 0;
     } else if (strcmp(op, "adds") == 0) {
@@ -16,26 +16,26 @@ static uint32_t build_dp_reg_arithmetic(Instruction instr) {
     }
 
     // rd - bits 0-4
-    char* rd_str = instr.o1.reg;
+    char* rd_str = instr->o1.reg;
     uint32_t rd = atoi(rd_str + 1);
 
     // sf - bit 31
     uint32_t sf = rd_str[0] == 'x';
 
     // rn - bits 5-9
-    char* rn_str = instr.o2.reg;
+    char* rn_str = instr->o2.reg;
     uint32_t rn = atoi(rn_str + 1);
 
     // rm - bits 16-20
-    char* rm_str = instr.o3.reg;
+    char* rm_str = instr->o3.reg;
     uint32_t rm = atoi(rm_str + 1);
 
     // operand - bits 10-15
-    uint32_t operand = instr.o4.number;
+    uint32_t operand = instr->o4.number;
 
     // shift - bits 22-23
     uint32_t shift;
-    switch (instr.o4_type) {
+    switch (instr->o4_type) {
         case LSL:
             shift = 0;
             break;
@@ -56,10 +56,10 @@ static uint32_t build_dp_reg_arithmetic(Instruction instr) {
     return (sf << 31) | (opc << 29) | (5 << 25) | (opr << 21) | (rm << 16) | (rn << 5) | rd;
 }
 
-static uint32_t build_dp_reg_logical(Instruction instr) {
+static uint32_t build_dp_reg_logical(Instruction* instr) {
     // opc - bits 29-30, N - bit 21
     uint32_t opc, N;
-    char* op = instr.operation;
+    char* op = instr->operation;
     if (strcmp(op, "and") == 0) {
         opc = 0;
         N = 0;
@@ -90,26 +90,26 @@ static uint32_t build_dp_reg_logical(Instruction instr) {
     }
 
     // rd - bits 0-4
-    char* rd_str = instr.o1.reg;
+    char* rd_str = instr->o1.reg;
     uint32_t rd = atoi(rd_str + 1);
     
     // sf - bit 31
     uint32_t sf = rd_str[0] == 'x';
 
     // rn - bits 5-9
-    char* rn_str = instr.o2.reg;
+    char* rn_str = instr->o2.reg;
     uint32_t rn = atoi(rn_str + 1);
 
     // rm - bits 16-20
-    char* rm_str = instr.o3.reg;
+    char* rm_str = instr->o3.reg;
     uint32_t rm = atoi(rm_str + 1);
 
     // operand - bits 10-15
-    uint32_t operand = instr.o4.number;
+    uint32_t operand = instr->o4.number;
 
     // shift - bits 22-23
     uint32_t shift;
-    switch (instr.o4_type) {
+    switch (instr->o4_type) {
         case LSL:
             shift = 0;
             break;
@@ -130,34 +130,34 @@ static uint32_t build_dp_reg_logical(Instruction instr) {
     return (sf << 31) | (opc << 29) | (5 << 25) | (opr << 21) | (rm << 16) | (operand << 10) | (rn << 5) | rd;
 }
 
-uint32_t build_dp_reg_multiply(Instruction instr) {
+uint32_t build_dp_reg_multiply(Instruction* instr) {
     // rd - bits 0-4
-    char* rd_str = instr.o1.reg;
+    char* rd_str = instr->o1.reg;
     uint32_t rd = atoi(rd_str + 1);
 
     // sf - bit 31
     uint32_t sf = rd_str[0] == 'x';
 
     // ra - bits 5-9
-    char* rn_str = instr.o2.reg;
+    char* rn_str = instr->o2.reg;
     uint32_t rn = atoi(rn_str + 1);
 
     // rm - bits 16-20
-    char* rm_str = instr.o3.reg;
+    char* rm_str = instr->o3.reg;
     uint32_t rm = atoi(rm_str + 1);
 
     // ra - bits 10-14
-    char* ra_str = instr.o4.reg;
+    char* ra_str = instr-.o4.reg;
     uint32_t ra = atoi(ra_str + 1);
 
     // x - bit 15
-    uint32_t x = strcmp(instr.operation, "msub") == 0;
+    uint32_t x = strcmp(instr->operation, "msub") == 0;
 
     return (sf << 31) | (216 << 21) | (rm << 16) | (x << 15) | (ra << 10) | (rn << 5) | rd;
 }
 
-uint32_t build_dp_reg(Instruction instr) {
-    char* op = instr.operation;
+uint32_t build_dp_reg(Instruction* instr) {
+    char* op = instr->operation;
 
     if (strstr(op, "add") != NULL || strstr(op, "sub") != NULL) {
         return build_dp_reg_arithmetic(instr);
@@ -166,7 +166,7 @@ uint32_t build_dp_reg(Instruction instr) {
     } else if (strstr(op, "madd") != NULL || strstr(op, "msub") != NULL) {
         return build_dp_reg_multiply(instr);
     } else {
-        printf("Invalid operation for DP register instruction\n");
+        printf("Invalid operation %s for DP register instruction\n", op);
         exit(1);
     }
 }
