7e8bb6e73c5cff1debf2d68b280042d8fbc77adb
ab4623 <ab4623@ic.ac.uk>
2024-06-04 15:09:38 +0100

    implemented dp imm assembler for logical instructions

--- src/dp-imm-assemble.c
+++ src/dp-imm-assemble.c
@@ -6,6 +6,8 @@ uint32_t build_dp_imm(Instruction instruction)
 
     if (strstr(operation, "add") != NULL  || strstr(operation, "sub") != NULL) {
         return build_dp_imm_arithmetic(instruction);
+    }else {
+        return build_dp_imm_logical(instruction);
     }
 }
 
@@ -40,4 +42,33 @@ uint32_t build_dp_imm_arithmetic(Instruction instruction)
     }
 
     return (sf << 31) | (opc << 29) | (4 << 26) | (opi << 23) | (sh << 22) | (imm12 << 10) | (rn << 5) | rd;
+}
+
+uint32_t build_dp_imm_logical(Instruction instruction)
+{
+    uint32_t opi = 3;
+    uint32_t opc;
+    char* operation = instruction.operation;
+
+    if (strcmp(operation, "movn") == 0) {
+        opc = 0;
+    }else if (strcmp(operation, "movz") == 0) {
+        opc = 2;
+    }else if (strcmp(operation, "movk") == 0) {
+        opc = 3;
+    }
+
+    char* rd_str = instruction.o1.reg;
+    uint32_t sf = rd_str[0] == 'x';
+    uint32_t rd = atoi(rd_str + 1);
+
+    uint32_t imm16 = instruction.o2.number;
+    uint32_t hw;
+
+    if (instruction.o3_type == LSL) {
+        int shift = instruction.o3.number;
+        hw = shift / 16;
+    }
+
+    return (sf << 31) | (opc << 29) | (4 << 26) | (opi << 23) | (hw << 21) | (imm16 << 5) | rd;
 }
