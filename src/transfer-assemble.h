188efbaa60974237e515fc72a36dc07f745822ae
andytam101 <andytam321@gmail.com>
2024-06-06 13:55:09 +0100

    transfer

--- src/transfer-assemble.h
+++ src/transfer-assemble.h
@@ -1,3 +1,25 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
 #include "parse-asm.h"
+
+#define SF_SHIFT 30
+#define LL_CONSTANT 24
+#define LL_CONSTANT_SHIFT 24
+#define SIMM19_SHIFT 5
+
+#define SDT_INDICATOR_SHIFT 31
+#define SDT_CONSTANT 28
+#define SDT_CONSTANT_SHIFT 25
+#define XN_SHIFT 5
+#define L_SHIFT 22
+#define U_SHIFT 24
+#define OFFSET_SHIFT 10
+
+#define XM_SHIFT 16
+#define REG_OFS_CONSTANT 13
+#define REG_OFS_CONSTANT_SHIFT 11
+
+#define SIMM9_SHIFT 2
+#define I_SHIFT 1
