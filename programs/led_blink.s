2fccd8b822f251730f502937641c5e69bf664e30
Ryan Ghoussainy <rg1523@oak25.doc.ic.ac.uk>
2024-06-13 22:18:30 +0100

    Finished led_blink.s

--- programs/led_blink.s
+++ programs/led_blink.s
@@ -1,39 +1,40 @@
 movz w0, #0x3f20, lsl #16
 
-movz w1, #0x1249
+movz w1, #0x40
 str w1, [w0]
 
 movz w1, #0x1c
 movz w2, #0x28
 
-orr w0, w0, w1
-orr w1, w0, w2
+orr w1, w0, w1
+orr w2, w0, w2
 
-movz w4, #0xffff
-movz w6, #0
-movz w5, #10000
+movz w3, #0
+movz w4, #4
 
-on:
-    str w6, [w1]
-    str w4, [w0]
-    sub w5, w5, #1
-    cmp w5, #0
-    b.eq onToOff
-    b on
+movz w5, #0x003c, lsl #16
 
 off:
-    str w6, [w0]
+    str w3, [w1]
+    str w4, [w2]
+    sub w5, w5, #1
+    tst w5, w5
+    b.eq onResetTimer
+    b off
+
+on:
+    str w3, [w2]
     str w4, [w1]
     sub w5, w5, #1
-    cmp w5, #0
-    b.eq offToOn
-    b off
-
-onToOff:
-    movz w5, #10000
-    b off
-
-offToOn:
-    movz w5, #10000
+    tst w5, w5
+    b.eq offResetTimer
     b on
+
+onResetTimer:
+    movz w5, #0x003c, lsl #16
+    b on
+
+offResetTimer:
+    movz w5, #0x003c, lsl #16
+    b off
 
