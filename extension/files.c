48a039800ce8f39cd1f563854abacdfeea2df64c
Ryan Ghoussainy <rg1523@oak25.doc.ic.ac.uk>
2024-06-16 22:52:17 +0100

    Added free_dir function

--- extension/files.c
+++ extension/files.c
@@ -5,5 +5,16 @@ void free_file(File* file) {
 
     free(file->name);
     free(file->content);
+    free(file->path);
     free(file);
+}
+
+void free_dir(Directory* dir) {
+    assert(dir != NULL);
+
+    free(dir->name);
+    if (dir->files != NULL)       dir->files->free_elem(dir->files);
+    if (dir->directories != NULL) dir->directories->free_elem(dir->directories);
+    free(dir->path);
+    free(dir);
 }
