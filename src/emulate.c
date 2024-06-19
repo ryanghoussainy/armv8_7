9538ff8a6e7de4c73a10c45ac76ee70661a95f7e
Ryan Ghoussainy <rg1523@oak25.doc.ic.ac.uk>
2024-06-19 11:06:13 +0100

    Removed unnecessary memory allocation for file names in emulator

--- src/emulate.c
+++ src/emulate.c
@@ -5,53 +5,19 @@
 
 int main(int argc, char **argv) {
 
-  char *FILE_IN = malloc(20 * sizeof(char));
-  assert(FILE_IN != NULL);
-  char *FILE_OUT = malloc(20 * sizeof(char));
-  assert(FILE_OUT != NULL);
+  char* FILE_IN;
+  char* FILE_OUT;
 
-  // Need to dynamically allocate memory for large filenames
-
-  if (argc == 2 || argc == 3) {
-
-    if (strlen(argv[1]) >= (20 * sizeof(char))) {
-
-      // If the filename is larger than 20 characters then reallocate to accommodate
-
-      FILE_IN = (char *) realloc(FILE_IN, (strlen(argv[1]) + 1) * sizeof(char));
-      assert(FILE_IN != NULL);
-
-    }
-
-    strcpy(FILE_IN, argv[1]);
-    printf("Input file is %s\n", FILE_IN);
-
-    if (argc == 3) {
-
-      if (strlen(argv[2]) >= (20 * sizeof(char))) {
-
-        FILE_OUT = (char *) realloc(FILE_OUT, (strlen(argv[2]) + 1) * sizeof(char));
-        assert(FILE_OUT != NULL);
-      }
-
-      strcpy(FILE_OUT, argv[2]);
-      printf("Output file is %s\n", FILE_OUT);
-
-    } else {
-
-      strcpy(FILE_OUT, "stdout");
-
-    }
-
+  if (argc == 2) {
+    FILE_IN = argv[1];
+    FILE_OUT = "stdout";
+  } else if (argc == 3) {
+    FILE_IN = argv[1];
+    FILE_OUT = argv[2];
   } else {
-
     printf("Wrong number of arguments supplied\n");
-    free(FILE_IN);
-    free(FILE_OUT);
     return EXIT_FAILURE;
-
   }
-
   
   CPU cpu;
 
@@ -59,8 +25,6 @@ int main(int argc, char **argv) {
 
   // Load binary file into memory
   if (!load_binary(FILE_IN, cpu.memory)) {
-    free(FILE_IN);
-    free(FILE_OUT);
     return EXIT_FAILURE;
   }
 
@@ -77,9 +41,6 @@ int main(int argc, char **argv) {
     print_cpu(&cpu, out);
 
   }
-
-  free(FILE_IN);
-  free(FILE_OUT);
 
   return EXIT_SUCCESS;
 }
